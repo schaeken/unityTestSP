f873c50abca15cafd1df8307aef13c2938b101f4
Kevin Schaeken <kevin.schaeken@gmail.com>
2014-01-31 11:01:50 -0500

    put unity test on github

---
+++ Assets/Standard Assets/Character Controllers/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float movementSpeed = 10;
+	public float turningSpeed = 10;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis ("Vertical") * turningSpeed * Time.deltaTime;
+		transform.Rotate (0, horizontal, 0);
+		float vertical = Input.GetAxis ("Horizontal") * movementSpeed * Time.deltaTime;
+		transform.Translate (vertical,0 ,0);
+
+	}
+}
---
+++ Assets/Standard Assets/Character Controllers/dungeonCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dungeonCamera : MonoBehaviour {
+	public GameObject target;
+	public float damping = 1;
+	Vector3 offset;
+
+	void Start(){
+		offset = transform.position - target.transform.position;
+	}
+	void LateUpdate(){
+		Vector3 desiredPosition = target.transform.position + offset;
+		Vector3 position = Vector3.Lerp(transform.position,desiredPosition,Time.deltaTime *damping);
+		transform.position = desiredPosition;
+		transform.LookAt (target.transform.position);
+	}
+}
